1fefb20753b839d553d48af0140085e34a9e093a
tslusher21 <tslusher21@gmail.com>
2015-11-08 19:36:14 -0500

    One last fix to plot 4

--- plot4.R
+++ plot4.R
@@ -21,17 +21,17 @@ png("project/plot4.png", width=480, height=480)
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(plotData, {
   plot(Global_active_power~Datetime, type="l", 
-       ylab="Global Active Power (kilowatts)", xlab="")
+       ylab="Global Active Power", xlab="")
   plot(Voltage~Datetime, type="l", 
-       ylab="Voltage (volt)", xlab="")
+       ylab="Voltage (volt)", xlab="datetime")
   plot(Sub_metering_1~Datetime, type="l", 
-       ylab="Global Active Power (kilowatts)", xlab="")
+       ylab="Global Active Power", xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Global_reactive_power~Datetime, type="l", 
-       ylab="Global Rective Power (kilowatts)",xlab="")
+       ylab="Global Rective Power",xlab="datetime")
 })
 
 dev.off()
